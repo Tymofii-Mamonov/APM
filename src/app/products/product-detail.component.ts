0b3868fc63049e5ff85d94629546b1d02b3a3b90
Tymofii Mamonov <tymofii.mamonov@lebara.com>
2018-05-06 22:22:48 +0300

    First commit

---
+++ src/app/products/product-detail.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './product.service';
+
+@Component({
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  pageTitle: string  = 'Product Detail';
+  product: IProduct;
+
+  constructor(private _route: ActivatedRoute,
+              private _router: Router,
+              private _productService: ProductService) { }
+
+  ngOnInit() {
+    let id = +this._route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this._productService.getProducts()
+        .subscribe(products => 
+        {
+          this.product = products.find(product => product.productId == id)
+        },
+        error => <any>error);
+  }
+
+  onBack(): void {
+    this._router.navigate(["/products"]);
+  }
+
+}
